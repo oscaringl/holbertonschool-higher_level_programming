d361f02d486e3007b615fa93896d74a98c0aa58b
Oscar Guerra <oogl29@gmail.com>
2023-01-27 23:28:02 -0500

    Update a function 10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
+
+
 def divisible_by_2(my_list=[]):
-    if my_list:
-        values = []
-        for i in my_list:
-            if i % 2 is 0:
-                values.append(True)
-            else:
-                values.append(False)
-        return values
+    return [not i % 2 for i in my_list]
