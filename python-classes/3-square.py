9ef8dc45218f9638486a85b53616dc4f7bed6696
Oscar Guerra <oogl29@gmail.com>
2023-02-03 21:01:03 -0500

    Update a 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 class Square:
-    """Defines a square"""
+    """ A class that defines a square by its size
+    """
     def __init__(self, size=0):
-        """Initialises the data"""
-        self.__size = size
-        if type(size) != int:
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
-        """Returns current square area"""
-        return self.__size**2
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
