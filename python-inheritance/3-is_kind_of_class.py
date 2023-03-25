0f3230a4e18a40969981a1f9aada259f04f36910
Oscar Guerra <oogl29@gmail.com>
2023-03-25 02:33:03 -0500

    Create a 3-is_kind_of_class.py file

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Module with method is the type of the class"""
 
 def is_kind_of_class(obj, a_class):
     """ Method that returns True if the object is an instance of a class, or if the object is
