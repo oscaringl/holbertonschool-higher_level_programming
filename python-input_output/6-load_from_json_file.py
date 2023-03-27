033f9bade60e2b6e709c49f62b92217cffea44a2
Oscar Guerra <oogl29@gmail.com>
2023-03-27 08:52:38 -0500

    Update a 6-load_from_json_file.py file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Function load_from_json_file"""
+"""Function load_from_json_file()"""
 import json
 
 
 def load_from_json_file(filename):
-    """function that creates an Object from a “JSON file"""
-
-    with open(filename, mode="r", encoding='utf-8') as my_file:
-        return json.load(my_file)
+    """function that creates an Object from a “JSON file”"""
+    with open(filename, "r", encoding="UTF8") as f:
+        return json.loads(f.read())
