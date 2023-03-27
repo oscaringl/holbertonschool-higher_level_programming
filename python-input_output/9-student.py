b6bd9aa935e7bbdde9a6da500f7e7293f0495564
Oscar Guerra <oogl29@gmail.com>
2023-03-27 08:59:38 -0500

    Update a 9-student.py file

---
+++ python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function Defines "Student" class"""
+
+
+class Student:
+    """Student """
+    def __init__(self, first_name, last_name, age):
+        """Initializes student's information"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Method that retrieves a dictionary representation of a Student instance """
+        return self.__dict__
