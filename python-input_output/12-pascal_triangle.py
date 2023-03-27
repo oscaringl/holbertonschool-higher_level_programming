25c7adba2d69ecdb96067a4a0cb23cee43375897
Oscar Guerra <oogl29@gmail.com>
2023-03-27 09:16:31 -0500

    Create a 12-pascal_triangle.py file

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function def pascal_triangle(n)"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = []
+    row = []
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            for j in range(i):
+                row.append(sum(triangle[-1][j:j+2]))
+        triangle.append(row)
+    return triangle
