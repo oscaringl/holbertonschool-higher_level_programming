04d544cb8a8e57a294d977b6e2200584d6196fab
Oscar Guerra <oogl29@gmail.com>
2023-04-28 21:36:29 -0500

    Create file 7-script.js

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+/* Fetches the name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json */
+
+const $ = window.$;
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('div#character').html(data.name);
+});
