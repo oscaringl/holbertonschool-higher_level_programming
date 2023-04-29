e364056c6d739f6eb23be9bf2855fcadbf3b06a1
Oscar Guerra <oogl29@gmail.com>
2023-04-28 20:45:31 -0500

    Create file 100-starwars_characters.js

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/**
+ * Prints all characters of specified Star Wars movie.
+ */
+const request = require('request');
+request.get('http://swapi.co/api/films/' + process.argv[2], (err, resp, body) => {
+  if (err) console.log(err);
+  else if (resp.statusCode === 200) {
+    let film = JSON.parse(body);
+    for (let ch of film.characters) {
+      request.get(ch, (err, resp, body) => {
+        if (err) console.log(err);
+        else if (resp.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
