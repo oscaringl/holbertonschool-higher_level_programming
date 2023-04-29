1187385c9af8152ffb1340c2a498ee7dd44508d4
Oscar Guerra <oogl29@gmail.com>
2023-04-28 20:41:37 -0500

    Create file 5-request_store.js

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * Get contents of a webpage and stores it in a file.
+ */
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], function (error, response, body) {
+  if (response.statusCode === 200) {
+    fs.writeFile(process.argv[3], body, 'utf-8', function (error) {
+      if (error) console.log(error);
+    }
+    );
+  } else {
+    console.log(error);
+  }
+});
