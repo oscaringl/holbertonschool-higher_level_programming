193db4526beafe3461bc2cdda3551d00c924a5bd
Oscar Guerra <oogl29@gmail.com>
2023-04-28 20:43:17 -0500

    Create file 6-completed_tasks.js

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/** Compute the number of tasks completed by user id.
+ */
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    for (const todo in todos) {
+      const task = todos[todo];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log(error);
+  }
+});
