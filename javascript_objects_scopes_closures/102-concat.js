30a5323a1d3a55e50370d5b2c942da4fdf0f3932
Oscar Guerra <oogl29@gmail.com>
2023-04-28 22:00:20 -0500

    Update file 102-concat.js

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+
+let fileA = process.argv[2];
+let fileB = process.argv[3];
+let fileC = process.argv[4];
 const fs = require('fs');
-let args = process.argv.slice(2);
-if (args.length < 3) {
-  console.log('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-let fa = fs.readFileSync(args[0]);
-let fb = fs.readFileSync(args[1]);
-fs.writeFileSync(args[2], fa + fb);
+let textA = fs.readFileSync(fileA, 'utf8');
+let textB = fs.readFileSync(fileB, 'utf8');
+fs.writeFileSync(fileC, textA + textB);
