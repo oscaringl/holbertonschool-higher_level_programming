af032a2b88eefbc4353ed70c9071d599655d5949
Oscar Guerra <oogl29@gmail.com>
2023-04-28 21:52:42 -0500

    Create file 103-script.js

---
+++ javascript-web_jquery/103-script.js
@@ -0,0 +1,19 @@
+/* Fetches and prints how to say “Hello” depending on the language */
+const $ = window.$;
+window.onload = function () {
+  $('INPUT#btn_translate').click(function () {
+    showHello();
+  });
+  $('INPUT#language_code').keypress(function (event) {
+    if (event.keyCode === 13) {
+      showHello();
+    }
+  });
+};
+
+function showHello () {
+  const lan = $('INPUT#language_code').val();
+  $.get('https://fourtonfish.com/hellosalut/?lang=' + lan, function (data, textStatus) {
+    $('DIV#hello').text(data.hello);
+  });
+}
