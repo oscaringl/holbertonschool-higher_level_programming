a1fa6e7ac95dc0b608152166e6964e7b96e7e8a8
Oscar Guerra <oogl29@gmail.com>
2023-04-28 20:31:09 -0500

    Create file 3-starwars_title.js

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * Print the title of a Star Wars movie.
+ */
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const request = require('request');
+
+request(url + process.argv[2], function (error, response, body) {
+  if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.log(error);
+  }
+});
