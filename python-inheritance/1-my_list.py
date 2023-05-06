bf7cfcae5c496513a099790e798ddd10a94d8cee
Oscar Guerra <oogl29@gmail.com>
2023-05-05 22:06:55 -0500

    Update folder

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""Contains class that inherits from list"""
+"""
+Module 1-my_list
+
+Contains class MyList
+inherits from list; has public instance method to print sorted
+"""
 
 
 class MyList(list):
-    """Class inherits from list"""
+    """inherits from list
 
+    methods:
+    print_sorted(self)
+    """
     def print_sorted(self):
         """prints list of ints all sorted in ascending order"""
         print(sorted(self))
